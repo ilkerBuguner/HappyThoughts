c1f8d54f5bdfba07dd696197ffcb5b0ceab56f86
İlker Bugüner <ilker.buguner@gmail.com>
2020-02-24 13:48:56 +0200

    Adds topic create page and a Create Topic button in the  navigation.

---
+++ HappyThoghts/Web/HappyThoughts.Web/Controllers/TopicsController.cs
@@ -0,0 +1,22 @@
+﻿namespace HappyThoughts.Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public class TopicsController : BaseController
+    {
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        public IActionResult Details()
+        {
+            return this.View();
+        }
+    }
+}
